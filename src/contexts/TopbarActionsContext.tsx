732dbeef65436d38d69eb93083b90d5458fca5af
Guillaume Delré <delre.guillaume@gmail.com>
2026-06-06 22:05:21 +0200

    fix(lint): disable react-refresh rule on useTopbarActions hook export

--- src/contexts/TopbarActionsContext.tsx
+++ src/contexts/TopbarActionsContext.tsx
@@ -17,6 +17,7 @@ export function TopbarActionsProvider({ children }: { children: ReactNode }) {
   )
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useTopbarActions() {
   return useContext(TopbarActionsContext)
 }
